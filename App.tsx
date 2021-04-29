e93307ef27e533e66e0702379a7b12456a4babe2
Raphael <raphael.petrere@etapaeducacional.onmicrosoft.com>
2021-04-29 16:07:01 -0300

    Criando a tela de confirmação de usuário e iniciando as rotas

--- App.tsx
+++ App.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
-import { Welcome } from './src/pages/Welcome';
-import { UserIdentification } from './src/pages/UserIdentification';
+import Routes from './src/routes';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -17,6 +16,6 @@ export default function App() {
     
 
   return (
-    <UserIdentification />
+    <Routes />
   )
 }
