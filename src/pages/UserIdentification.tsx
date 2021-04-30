42b393e37f7b91400efd2e6e08af22ab7aaa115f
Raphael <raphael.petrere@etapaeducacional.onmicrosoft.com>
2021-04-29 21:39:57 -0300

    Utilizando o iphone-x-helper para evitar o notch | Utilizando json-server e axios pra fake API | Utilizando Lottie para animação

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,5 +1,15 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, SafeAreaView, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { 
+    View, 
+    StyleSheet, 
+    SafeAreaView, 
+    Text, 
+    TextInput, 
+    KeyboardAvoidingView, 
+    TouchableWithoutFeedback, 
+    Platform, 
+    Keyboard
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from '../components/Button';
 import colors from '../styles/colors';
@@ -34,32 +44,34 @@ export function UserIdentification() {
     return (
         <SafeAreaView style={styles.container}>
             <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-                <View style={styles.content}>
-                    <View style={styles.form}>
-                        <View style={styles.header}>
-                            <Text style={styles.emoji}>
-                                {isFilled ? '😄' : '😀'}
-                            </Text>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <View style={styles.header}>
+                                <Text style={styles.emoji}>
+                                    {isFilled ? '😄' : '😀'}
+                                </Text>
 
-                            <Text style={styles.title}>
-                                Como podemos {'\n'} chamar você?
-                            </Text>
-                        </View>
-                        <TextInput 
-                            style={[
-                                styles.input,
-                                (isFocused || isFilled) && {borderColor: colors.green}
-                            ]} 
-                            placeholder="Digite um nome" 
-                            onBlur={handleInputBlur} 
-                            onFocus={handleInputFocus} 
-                            onChangeText={handleInputChange}
-                        />
-                        <View style={styles.footer}>
-                            <Button title="Confirmar" onPress={handleSubmit}/>
+                                <Text style={styles.title}>
+                                    Como podemos {'\n'} chamar você?
+                                </Text>
+                            </View>
+                            <TextInput 
+                                style={[
+                                    styles.input,
+                                    (isFocused || isFilled) && {borderColor: colors.green}
+                                ]} 
+                                placeholder="Digite um nome" 
+                                onBlur={handleInputBlur} 
+                                onFocus={handleInputFocus} 
+                                onChangeText={handleInputChange}
+                            />
+                            <View style={styles.footer}>
+                                <Button title="Confirmar" onPress={handleSubmit}/>
+                            </View>
                         </View>
                     </View>
-                </View>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,6 +5,7 @@ import colors from '../styles/colors';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { SelectPlant } from '../pages/SelectPlant';
 
 const stackRoutes = createStackNavigator();
 
@@ -20,6 +21,7 @@ const AppRoutes: React.FunctionComponent = () => (
         <stackRoutes.Screen name="Welcome" component={Welcome} />
         <stackRoutes.Screen name="UserIdentification" component={UserIdentification} />
         <stackRoutes.Screen name="Confirmation" component={Confirmation} />
+        <stackRoutes.Screen name="SelectPlant" component={SelectPlant} />
     </stackRoutes.Navigator>
 )
 
